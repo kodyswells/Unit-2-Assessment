d366eca48aa0f3c82ed63047c7f3284114655b75
Kody Wells <kodyswells@gmail.com>
2024-02-01 10:04:24 -0700

    Solved orders

--- orders.js
+++ orders.js
@@ -29,7 +29,18 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor(items,orderTime,customerId) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = `queued`;
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for ${this.customerId}, is ${this.status}.`)
+    }
+}
 
 
 
@@ -44,13 +55,14 @@
     customer: 575
 */
 
-//CODE HERE
+const firstTicket = new Ticket(`pizza, bread, soda`,`7:03 PM`, 575);
 
-
+console.log(firstTicket);
 /*
     Call the `updateStatus` method on
     `firstTicket` passing in the string
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus(`cooking`);
+console.log(firstTicket);
